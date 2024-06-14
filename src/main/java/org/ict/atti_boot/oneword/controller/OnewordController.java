8d32e6465129d45fb3b583f6d36e2e18606a7bfd
ictedu29 <tactius90@gmail.com>
2024-06-14 17:10:51 +0900

    오늘 한 줄 수정 / 2024-06-14 / 남길현

--- src/main/java/org/ict/atti_boot/oneword/controller/OnewordController.java
+++ src/main/java/org/ict/atti_boot/oneword/controller/OnewordController.java
@@ -3,10 +3,16 @@ package org.ict.atti_boot.oneword.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.ict.atti_boot.oneword.model.dto.OnewordDto;
+import org.ict.atti_boot.oneword.model.dto.OnewordSubjectDto;
 import org.ict.atti_boot.oneword.model.service.OnewordService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -15,6 +21,25 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin     //// 리액트 애플리케이션(포트가 다름)의 자원 요청을 처리하기 위함
 public class OnewordController {
     private final OnewordService onewordService;
+
+    @GetMapping("/list")
+    public ResponseEntity<List<OnewordDto>> selectList(@RequestParam(name="page", defaultValue = "0") int page,
+                                                              @RequestParam(name="size", defaultValue = "10") int size) {
+        log.info("/oneword/list : {}", page + ",  " + size);
+        //JPA 가 제공하는 Pageable 객체를 사용함
+        //Pageable pageable = PageRequest.of(page - 1, size, Sort.by(Sort.Direction.DESC, "owsjNum"));
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "owNum"));
+
+        //페이지에 출력할 목록 조회해 옴 => 응답 처리
+        return new ResponseEntity<>(onewordService.selectList(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping("/onedetail/{owNum}")
+    public ResponseEntity<OnewordDto> selectOnewordDetail(@PathVariable("owNum") int owNum){
+        //log.info("/boards/boardNum => selectBoardDetail()");
+        log.info("oneword/{} 요청", owNum);
+        return new ResponseEntity<>(onewordService.selectOnewordDetail(owNum), HttpStatus.OK);
+    }
 
     @PostMapping
     public ResponseEntity<?> insertOneword(@RequestBody OnewordDto onewordDto){
