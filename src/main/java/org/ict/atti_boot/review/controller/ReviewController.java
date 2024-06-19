726e8f0ca95655f02dba1ccf40286d4b690d2b68
ictedu29 <tactius90@gmail.com>
2024-06-19 10:17:17 +0900

    reviewController -> ReviewController로 수정 후 다시 추가 / 2024-06-19 / 남길현

---
+++ src/main/java/org/ict/atti_boot/review/controller/ReviewController.java
@@ -0,0 +1,50 @@
+package org.ict.atti_boot.review.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.ict.atti_boot.review.jpa.entity.Review;
+import org.ict.atti_boot.review.model.service.ReviewService;
+import org.ict.atti_boot.review.model.output.OutputReview;
+import org.ict.atti_boot.review.model.output.ReviewResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/review")
+@Slf4j
+public class ReviewController {
+    private final ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ReviewResponse> getMoreReview(@PageableDefault(size = 4, sort = "writeDate", direction = Sort.Direction.DESC) Pageable pageable, @RequestParam(name = "id") String doctorId) {
+        Page<Object[]> reviewData = reviewService.findByDoctorId(doctorId, pageable);
+        //리뷰 아웃풋 DTO 객체로 바꾸깅
+        List<OutputReview> reviewList = reviewData.getContent().stream()
+                .map(objects -> {
+                    Review review = (Review) objects[0];
+                    String nickName = (String) objects[1];
+                    return OutputReview.builder()
+                            .nickName(nickName)
+                            .content(review.getContent())
+                            .starPoint(review.getStarPoint())
+                            .build();
+                })
+                .collect(Collectors.toList());
+        Boolean hasMoreReview = reviewData.hasNext();
+
+        return ResponseEntity.ok(new ReviewResponse(reviewList, hasMoreReview));
+    }
+
+
+
+}
