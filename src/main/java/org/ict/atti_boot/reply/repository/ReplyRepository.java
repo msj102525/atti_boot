7339212504372c6b40d93d2917d627bfdee3d41f
msj102525 <msj102525@gmail.com>
2024-06-19 16:47:37 +0900

    FeedDetail 댓글 리스트 불러오기 - 문승종

--- src/main/java/org/ict/atti_boot/reply/repository/ReplyRepository.java
+++ src/main/java/org/ict/atti_boot/reply/repository/ReplyRepository.java
@@ -5,7 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface ReplyRepository extends JpaRepository<Reply, Integer> {
     Page<Reply> findAllByFeedNumOrderByReplyDateDesc(Pageable pageable, int feedNum);
+
+    List<Reply> findAllByReplyReplyRef(int replyNum);
+
 }
