27a53d577d5bc665c9ec3f13cef798b42e4556a2
msj102525 <msj102525@gmail.com>
2024-06-20 10:39:04 +0900

    gitignore 수정 -문승종

---
+++ src/main/generated/org/ict/atti_boot/admin/model/entity/QAdminEntity.java
@@ -0,0 +1,43 @@
+package org.ict.atti_boot.admin.model.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QAdminEntity is a Querydsl query type for AdminEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QAdminEntity extends EntityPathBase<AdminEntity> {
+
+    private static final long serialVersionUID = -563602866L;
+
+    public static final QAdminEntity adminEntity = new QAdminEntity("adminEntity");
+
+    public final StringPath email = createString("email");
+
+    public final StringPath nickName = createString("nickName");
+
+    public final StringPath userId = createString("userId");
+
+    public final StringPath userName = createString("userName");
+
+    public QAdminEntity(String variable) {
+        super(AdminEntity.class, forVariable(variable));
+    }
+
+    public QAdminEntity(Path<? extends AdminEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QAdminEntity(PathMetadata metadata) {
+        super(AdminEntity.class, metadata);
+    }
+
+}
+
