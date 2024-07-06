f038fcfa71256aec85051f46df22ad9a8504d76f
msj102525 <msj102525@gmail.com>
2024-07-06 15:20:08 +0900

    CORS 설정 수정 - 문승종

--- src/main/java/org/ict/atti_boot/config/WebConfig.java
+++ src/main/java/org/ict/atti_boot/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
         // 모든 경로에 대해 CORS 설정을 추가합니다. (/**는 모든 경로를 의미합니다.)
         registry.addMapping("/**")
                 // 개발 단계에서는 일반적으로 프론트엔드 서버의 주소가 됩니다.
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://43.202.66.137:3000", "http://www.attiservice.shop:3000")
                 // 해당 오리진에서 허용할 HTTP 메소드를 지정합니다.
                 // GET, POST, PUT, DELETE, HEAD, OPTIONS 메소드를 허용합니다.
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
